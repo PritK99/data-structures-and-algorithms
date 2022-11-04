2bb9a4ba2cdb5c852606f42a0870eca82652c3b7
PritK99 <pritpjk@gmail.com>
2022-11-05 04:32:03 +0530

    To check given bracket indentation is valid or not

---
+++ Stack/Miscellaneous/Balancing_brackets.cpp
@@ -0,0 +1,105 @@
+/*Here , we accepts an input string of brackets and cehck if the given indentation of brackets is valid or not*/
+
+#include <iostream>
+#define MAX_SIZE 10000
+
+using namespace std;
+
+class Stack
+{
+private:
+    char stack[MAX_SIZE];
+    int top = -1;
+
+public:
+    void push(char element);
+    char pop();
+    char peek();
+};
+
+/*This function allows us to insert an element in stack*/
+void Stack ::push(char element)
+{
+    if (top != MAX_SIZE - 1) // We can not push anymore elements if the stack is full
+    {
+        top++;
+        stack[top] = element;
+    }
+}
+// end of function
+
+/*This function removes the topmost element of stack
+if the function returns -1 , it implies stack is empty*/
+char Stack ::pop() // We can not pop any elements if the stack is empty
+{
+    if (top != -1)
+    {
+        return stack[top--];
+        /* We need to do top-- above itself cause the staements below the return statement are not executed*/
+    }
+    else
+    {
+        return 'X';
+    }
+}
+// end of function
+
+/*This function simply returns the topmost element of stack without deleting it from the stack
+if the function returns -1 , it implies stack is empty*/
+char Stack ::peek()
+{
+    if (top != -1)
+    {
+        return stack[top]; // here, we simply return the topmost element and not decrement top
+    }
+    else
+    {
+        return 'X';
+    }
+}
+// end of function
+
+int main()
+{
+    Stack s1;
+
+    string s;
+    getline(cin, s);
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == ')' && s1.peek() == '(')
+        {
+            char x;
+            x = s1.pop();
+        }
+        else if (s[i] == ']' && s1.peek() == '[')
+        {
+            char x;
+            x = s1.pop();
+        }
+        else if (s[i] == '}' && s1.peek() == '{')
+        {
+            char x;
+            x = s1.pop();
+        }
+        else
+        {
+            s1.push(s[i]);
+        }
+    }
+
+    // if stack is empty by now, the expression is valid
+    char x;
+    x = s1.peek();
+    if (x == 'X')
+    {
+        cout << "YES" << "\n";
+    }
+    else
+    {
+        cout << "NO" << "\n";
+    }
+
+    return 0;
+}
+// end of main
