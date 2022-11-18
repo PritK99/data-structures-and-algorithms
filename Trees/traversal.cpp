b50085993478eabfd6416fc4423ad37d35dfee06
PritK99 <pritpjk@gmail.com>
2022-11-18 08:11:29 +0530

    Binary trees

---
+++ Trees/traversal.cpp
@@ -0,0 +1,94 @@
+/*In this program, we perform traversals on Binary sreach tree recursively
+Note :-
+1)The BST is not a balanced tree 
+2) All the traversals take o(n) time complexity
+3) Insertion takes o(log(n)) time averagely, since this is not a BST. 
+*/
+#include <iostream>
+
+using namespace std;
+
+void insert(struct node *&node, int element);
+void inorder(struct node* root) ;
+void preorder(struct node* root) ;
+void postorder(struct node* root) ;
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *root = NULL; // The pointer to the staring node . for now this is null and will point to the root node when created
+
+int main()
+{
+    /*insertion*/
+    insert(root, 15);
+    insert(root, 5);
+    insert(root , 2) ;
+    insert(root , 40) ;
+    insert(root , 80) ;
+    insert(root , 20) ;
+    insert(root , 1) ;
+    insert(root , 3) ;
+
+    /*traversing*/
+    printf("The inorder Traversal is : ") ;
+    inorder(root) ;
+    printf("\n") ;
+    printf("The preorder Traversal is : ") ;
+    preorder(root) ;
+    printf("\n") ;
+    printf("The postorder Traversal is : ") ;
+    postorder(root) ;
+    printf("\n") ;
+}
+
+void insert(struct node * &node, int element)
+{
+    if (node == NULL) // if there is no root node i.e. the tree is empty
+    {
+        struct node *temp = (struct node *)malloc(sizeof(struct node)); // creating a memory in heap consisting a node
+        node = temp;
+        node->data = element;
+        node->left = NULL;
+        node->right = NULL; // here we create a new node
+    }
+    else if (element > node->data) // insert in right tree
+    {
+        insert(node->right, element);
+    }
+    else // insert in left tree
+    {
+        insert(node->left, element);
+    }
+}
+
+void inorder(struct node* root)
+{
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void preorder(struct node* root)
+{
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct node* root)
+{
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
