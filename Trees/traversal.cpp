e4c97eeeaead2511f2bba260354243cc36277e1e
PritK99 <pritpjk@gmail.com>
2023-01-01 09:35:27 +0530

    Organized the code in classes and methods

--- Trees/traversal.cpp
+++ Trees/traversal.cpp
@@ -1,52 +1,31 @@
 /*In this program, we perform traversals on Binary sreach tree recursively
 Note :-
-1)The BST is not a balanced tree 
+1)The BST is not a balanced tree
 2) All the traversals take o(n) time complexity
-3) Insertion takes o(log(n)) time averagely, since this is not a BST. 
+3) Insertion takes o(log(n)) time averagely, since this is not a BST.
 */
 #include <iostream>
 
 using namespace std;
 
-void insert(struct node *&node, int element);
-void inorder(struct node* root) ;
-void preorder(struct node* root) ;
-void postorder(struct node* root) ;
-
-struct node
+class Tree
 {
-    int data;
-    struct node *left;
-    struct node *right;
+    struct node
+    {
+        int data;
+        struct node *left = NULL;
+        struct node *right = NULL;
+    };
+
+public:
+    struct node *root = NULL; // The pointer to the staring node . for now this is null and will point to the root node when created
+    void insert(struct node *&node, int element);
+    void inorder(struct node *root);
+    void preorder(struct node *root);
+    void postorder(struct node *root);
 };
 
-struct node *root = NULL; // The pointer to the staring node . for now this is null and will point to the root node when created
-
-int main()
-{
-    /*insertion*/
-    insert(root, 15);
-    insert(root, 5);
-    insert(root , 2) ;
-    insert(root , 40) ;
-    insert(root , 80) ;
-    insert(root , 20) ;
-    insert(root , 1) ;
-    insert(root , 3) ;
-
-    /*traversing*/
-    printf("The inorder Traversal is : ") ;
-    inorder(root) ;
-    printf("\n") ;
-    printf("The preorder Traversal is : ") ;
-    preorder(root) ;
-    printf("\n") ;
-    printf("The postorder Traversal is : ") ;
-    postorder(root) ;
-    printf("\n") ;
-}
-
-void insert(struct node * &node, int element)
+void Tree :: insert(struct node *&node, int element)
 {
     if (node == NULL) // if there is no root node i.e. the tree is empty
     {
@@ -66,29 +45,58 @@ void insert(struct node * &node, int element)
     }
 }
 
-void inorder(struct node* root)
+void Tree :: inorder(struct node *root)
 {
-    if (root != NULL) {
+    if (root != NULL)
+    {
         inorder(root->left);
         printf("%d ", root->data);
         inorder(root->right);
     }
 }
 
-void preorder(struct node* root)
+void Tree :: preorder(struct node *root)
 {
-    if (root != NULL) {
+    if (root != NULL)
+    {
         printf("%d ", root->data);
         preorder(root->left);
         preorder(root->right);
     }
 }
 
-void postorder(struct node* root)
+void Tree :: postorder(struct node *root)
 {
-    if (root != NULL) {
+    if (root != NULL)
+    {
         postorder(root->left);
         postorder(root->right);
         printf("%d ", root->data);
     }
 }
+
+int main()
+{
+    Tree t;
+    /*insertion*/
+    t.insert(t.root, 15);
+    t.insert(t.root, 5);
+    t.insert(t.root, 2);
+    t.insert(t.root, 40);
+    t.insert(t.root, 80);
+    t.insert(t.root, 20);
+    t.insert(t.root, 1);
+    t.insert(t.root, 3);
+
+    /*traversing*/
+    printf("The inorder Traversal is : ");
+    t.inorder(t.root);
+    printf("\n");
+    printf("The preorder Traversal is : ");
+    t.preorder(t.root);
+    printf("\n");
+    printf("The postorder Traversal is : ");
+    t.postorder(t.root);
+    printf("\n");
+}
+
