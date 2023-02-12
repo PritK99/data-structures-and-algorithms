822241b21aa41432d812f7d3796870463e4e108e
PritK99 <pritpjk@gmail.com>
2023-02-12 20:48:08 +0530

    Adding theory for count sort

--- 0_Algorithms/Sorting/Readme.md
+++ 0_Algorithms/Sorting/Readme.md
@@ -8,6 +8,8 @@
 * Sorting Algorithms
     * Selection Sort
     * Merge Sort
+    * Count Sort
+    * Quick Sort
 * Conclusion
 
 ## Sorting Algorithms
@@ -24,10 +26,16 @@ Merge sort is a <b>comparison based sorting algorithm</b> based on <b>divide and
 
 In merge sort, we split the given array recursively and then sort them indivisually and merge them back.
 
+### <b>3) Count Sort</b>
+Count sort is a <b>Non-comparison based sorting algorithm</b> and has better time complexity than comparison based algorithms in general.
+
+However, for this algorithm to work, we need to deal with a <b>fixed range</b> of integers only, say I know my input is a mix of integers in range 0-100, then count sort is appropriate. 
+
+### <b>4) Quick Sort</b>
 ## <b>Conclusion</b>
 
 The time complexities of a few sorting algorithms are given below
 
 ![image](https://user-images.githubusercontent.com/103832825/218310768-d7d0b13e-bfe6-4f6b-a8fe-dd753132682a.png)
 
-For visualization of the above sorting algorithms, refer : https://www.toptal.com/developers/sorting-algorithms OR https://www.cs.usfca.edu/~galles/visualization/ComparisonSort.html.
+For visualization of the above sorting algorithms, refer : https://www.toptal.com/developers/sorting-algorithms and https://www.cs.usfca.edu/~galles/visualization/ComparisonSort.html.
---
+++ 0_Algorithms/Sorting/count_sort.cpp
@@ -0,0 +1,62 @@
+/*In this program, we will sort a list of elements using Count sort*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n ;
+
+    string s; 
+    getline(cin, s);
+
+    int num = 0;
+    vector<int> v;
+
+    /*Taking input from the user and storing it in a vector of int*/
+    for(int i = 0; i < s.size(); ++i)
+    {
+        if(s[i] == ' ')
+            v.push_back(num), num = 0;
+        else
+            num = num*10 + (s[i]-'0');
+    }   
+    v.push_back(num);
+
+    int max = *max_element(v.begin(), v.end()) ;
+    vector<int> range(max+1, 0) ;
+
+    /*This calculates the number of occurences*/
+    for (int i = 0 ; i < v.size() ; i++)
+    {
+        range[v[i]] ++ ;
+    }
+
+    /*This calculates the starting position of each integer*/
+    for (int i = 1 ; i < range.size() ; i++)
+    {
+        range[i] = range[i] + range[i-1] ;
+    }
+
+    /*Shift the array to left side*/
+    rotate(range.begin(), range.begin()+range.size() - 1, range.end());
+    range[0] = 0 ;
+
+    /*copy the elements from v to result vector as per their positions*/
+    vector<int> result(v.size());
+    for (int i = 0 ; i < v.size(); i++)
+    {
+        result[range[v[i]]] = v[i] ;
+        range[v[i]] ++ ;
+    }
+
+    /*display the result*/
+    for (int i = 0 ; i < result.size() ; i++)
+    {
+        cout << result[i] << " " ;
+    }
+    cout << endl ;
+
+}
