0fb965a6e08723e9c1c1057273732680c57affef
PritK99 <pritpjk@gmail.com>
2023-03-15 16:54:17 +0530

    Adding a gitignore file

---
+++ 0_Algorithms/1_Greedy_Algorithms/huffman.cpp
@@ -0,0 +1,23 @@
+/*******************************************************
+    @brief        Huffman encoding using greedy strategy
+*******************************************************/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void huffman(string s)
+{
+
+}
+
+int main()
+{
+    string s ;
+    cout << "Please enter the message: " ;
+    cin >> s ;
+    cout << "Sending message to reciever: " ;
+    huffman(s) ;
+
+}
