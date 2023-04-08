7be585f4631c6e6468ef9cab6e770c1be6176ff7
PritK99 <pritpjk@gmail.com>
2023-04-08 07:32:44 +0530

    Adding nearest neighbour and next

--- 5_Trees/binary_search_tree.cpp
+++ 5_Trees/binary_search_tree.cpp
@@ -2,6 +2,7 @@
     @brief        Implementing binary trees and basic operations
 ****************************************************************/
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -21,9 +22,11 @@ public:
     void inorder(struct node *&node);
     // void remove(struct node *&node, int element);
     bool find(struct node *&node, int key);
-    int next(struct node *&node, int key);
+    int next(struct node *node, int key);
     int leftDescendant(struct node *&node);
-    // int RightAncestors(struct node *&node);
+    int RightAncestors(struct node *&node);
+    void range(struct node *node, int lower, int upper);
+    void nearestNeighbour(struct node *node, int key) ;
 };
 void Tree::inorder(struct node *&node)
 {
@@ -35,8 +38,22 @@ void Tree::inorder(struct node *&node)
     }
 }
 
-int Tree :: next(struct node *&node, int key)
+int Tree :: next(struct node *node, int key)
 {
+    //Traverse to key node
+    while (node->data != key)
+    {
+        if (node->data > key)
+        {
+            node = node->left ;
+        }
+        else
+        {
+            node = node-> right ;
+        }
+    }
+
+    //Next element is either a left descendant of right child node or an ancestor with greater value than node.
     int x ;
     if (node->right != NULL)
     {
@@ -44,7 +61,7 @@ int Tree :: next(struct node *&node, int key)
     }
     else
     {
-        cout << "SORRY!\n" ;
+        x = RightAncestors(node) ;
     }
 
     return x ;
@@ -57,6 +74,113 @@ int Tree::leftDescendant(struct node *&node)
         return node->data ;
     }
     return leftDescendant(node->left);
+}
+
+int Tree::RightAncestors(struct node *&node)
+{
+    if (node->parent != NULL)
+    {
+        if (node->data < node->parent->data)
+        {
+            return node->parent->data ;
+        }
+        return RightAncestors(node->parent) ;
+    }
+    return -1 ;
+}
+
+void Tree::range(struct node *node, int lower, int upper)
+{
+    cout << "The elements which lie in given range are: " ;
+
+    //Traversing to the nearest element
+    while (node->data != lower)
+    {
+        if (node->data > lower)
+        {
+            if (node->left == NULL)
+            {
+                break ;
+            }
+            node = node->left ;
+        }
+        else
+        {
+            if (node->right == NULL)
+            {
+                break ;
+            }
+            node = node-> right ;
+        }
+    }
+
+    //Printing all elements which lie in range
+    int x = node->data ;
+
+    while (x < upper && x != -1)
+    {
+        cout << x << " " ;
+        x = next(root, x) ;
+    }
+
+    cout << endl ;
+}
+
+void Tree::nearestNeighbour(struct node *node, int key) 
+{
+    //Traversing to the nearest element
+    while (node->data != key)
+    {
+        if (node->data > key)
+        {
+            if (node->left == NULL)
+            {
+                break ;
+            }
+            node = node->left ;
+        }
+        else
+        {
+            if (node->right == NULL)
+            {
+                break ;
+            }
+            node = node-> right ;
+        }
+    }
+
+    int a = INT_MAX, b =INT_MAX, c = INT_MAX ;
+    if (node->parent != NULL)
+    {
+        a = fabs(key - node->parent->data) ;
+    }
+    if (node->left != NULL)
+    {
+        b = fabs(key - node->left->data) ;
+    }
+    if (node->right != NULL)
+    {
+        c = fabs(key - node->right->data) ;
+    }
+
+    cout << "The nearest neighbour to given element is: " ;
+
+    int min = 0;
+
+    if (a < b && a < c)
+    {
+        cout << node->parent->data ;
+    }
+    else if (b < c)
+    {
+        cout << node->left->data ;
+    }
+    else
+    {
+        cout << node->right->data ;
+    }
+
+    cout << endl ;
 }
 
 bool Tree ::find(struct node *&node, int key)
@@ -146,6 +270,7 @@ void Tree::insert(int element)
 int main()
 {
     Tree t;
+    int x ;
 
     //Insertion
     t.insert(15);
@@ -155,25 +280,28 @@ int main()
     t.insert(1);
 
     //Finding the element
-    // bool x = t.find(t.root, 131);
-    // if (x == 1)
-    // {
-    //     cout << "found\n";
-    // }
-    // else
-    // {
-    //     cout << "Element not found\n";
-    // }
-
-    //Inorder traversal
-    // t.inorder(t.root) ;
-
-    //Deletion
-    int x = t.next(t.root, 5) ;
-    cout << x << endl;
+    x = t.find(t.root, 131);
+    if (x == 1)
+    {
+        cout << "found\n";
+    }
+    else
+    {
+        cout << "Element not found\n";
+    }
 
     //Inorder traversal
     t.inorder(t.root) ;
+
+    //Next element
+    x = t.next(t.root, 5) ;
+    cout << x << endl;
+
+    //Range 
+    t.range(t.root, 1, 33) ;
+
+    //Nearest neighbour
+    t.nearestNeighbour(t.root, 12) ;
 
     return 0;
 }
