c7c8823fabef6befd2f801aa0db4c9b182ef31ae
PritK99 <pritpjk@gmail.com>
2023-05-06 10:10:56 +0530

    Adding 0/1 knapsack limited

--- 0_Algorithms/3_Dynamic_Programming/knapsack_limited.cpp
+++ 0_Algorithms/3_Dynamic_Programming/knapsack_limited.cpp
@@ -1,41 +1,60 @@
+/***************************************************************************
+    @brief        Using dynamic programming for 0/1 Knapsack_limited problem
+****************************************************************************/
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int knapsack(vector<vector<int>> &matrix, vector<int> price, vector<int> weight, int capacity, int n)
+/*
+ * Function Name: knapsack
+ * Input: Requires an vector of price, weights, status and values of capacity and n
+ * Output: returns the maximum profit
+ * Logic: Finds the optimal solution using bottom-to-top method of dynamic programming
+ * Example Call: knapsack(price, weight, capacity, n - 1, status)
+ */
+int knapsack(vector<int> price, vector<int> weight, int capacity, int n, vector<bool> &status)
 {
-    if (capacity == 0 || n < 0)
+    vector<vector<int>> matrix(weight.size() + 1, vector<int>(capacity + 1, 0));
+
+    for (int i = 1; i < matrix.size(); i++)
     {
-        return 0;
+        for (int j = 1; j < matrix[0].size(); j++)
+        {
+            if (weight[i - 1] <= j)
+            {
+                matrix[i][j] = max(matrix[i - 1][j], price[i - 1] + matrix[i - 1][j - weight[i - 1]]);
+            }
+            else
+            {
+                matrix[i][j] = matrix[i - 1][j];
+            }
+        }
     }
 
-    int x = 0, y = 0;
+    int x = weight.size();
+    int y = capacity;
+    int index = status.size() - 1;
 
-    if (weight[n] <= capacity)
+    while (x != 0)
     {
-        if (matrix[capacity - weight[n]][n] == 0)
+        if (matrix[x][y] == matrix[x - 1][y])
         {
-            x = price[n] + knapsack(matrix, price, weight, capacity - weight[n], n - 1);
-            matrix[capacity - weight[n]][n] = x;
+            status[index] = false;
+            x = x - 1;
+            index--;
         }
         else
         {
-            x = matrix[capacity - weight[n]][n];
+            status[index] = true;
+            x = x - 1;
+            y = y - weight[x];
+            index--;
         }
     }
 
-    if (matrix[capacity][n] == 0)
-    {
-        y = knapsack(matrix, price, weight, capacity, n - 1);
-        matrix[capacity][n] = y;
-    }
-    else
-    {
-        y = matrix[capacity][n];
-    }
-
-    return max(x, y);
+    return matrix[weight.size() - 1][capacity];
 }
 
 int main()
@@ -48,20 +67,34 @@ int main()
     cin >> n;
     vector<int> price(n);
     vector<int> weight(n);
+    vector<bool> status(n, false);
 
     for (int i = 0; i < n; i++)
     {
-        cout << "Enter weight of item " << i << " : ";
+        cout << "Enter weight of item " << i + 1 << " : ";
         cin >> weight[i];
-        cout << "Enter price of item " << i << " : ";
+        cout << "Enter price of item " << i + 1 << " : ";
         cin >> price[i];
     }
 
-    vector<vector<int>> matrix(capacity + 1, vector<int>(weight.size() + 1, 0));
+    int answer = knapsack(price, weight, capacity, n - 1, status);
 
-    int answer = knapsack(matrix, price, weight, capacity, n - 1);
-
-    cout << "Maximum profit: " << answer << endl;
+    for (int i = 0; i < status.size(); i++)
+    {
+        if (status[i] == true)
+        {
+            cout << "Item " << i + 1 << " : Taken";
+        }
+        else
+        {
+            cout << "Item " << i + 1 << " : Not Taken";
+        }
+        cout << endl;
+    }
+    cout << "Maximum profit is: " << answer << endl;
 
     return 0;
 }
+/*
+Analysis: The above code runs in O(m*n), where n is the number of different object required and m is the capacity of knapsack
+*/
