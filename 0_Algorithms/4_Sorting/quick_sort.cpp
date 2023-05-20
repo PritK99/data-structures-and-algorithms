2b7f5947d635a0aab37621abb383df5a6f8eafd8
PritK99 <pritpjk@gmail.com>
2023-05-20 22:50:35 +0530

    Adding initial implementation of quick sort

--- 0_Algorithms/4_Sorting/quick_sort.cpp
+++ 0_Algorithms/4_Sorting/quick_sort.cpp
@@ -1,13 +1,12 @@
-//Pending
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-using namespace std ;
+using namespace std;
 
 void Swap(int &i, int &j)
 {
-    int temp = i ;
+    int temp = i;
     i = j;
     j = temp;
 }
@@ -16,66 +15,70 @@ int Partition(vector<int> &v, int pivot, int i, int j)
 {
     if (i == j)
     {
-        return v[i] ;
+        return i;
     }
 
-    while (i < j)
+    while (i <= j)
     {
         if (v[i] > v[pivot] && v[j] < v[pivot])
         {
-            swap(v[i],v[j]) ;
-            i++ ;
-            j --;
+            swap(v[i], v[j]);
+            i++;
+            j--;
         }
         else if (v[i] <= v[pivot])
         {
-            i++ ;
+            i++;
         }
         else if (v[j] > v[pivot])
         {
-            j-- ;
+            j--;
         }
     }
 
-    Swap(v[pivot], v[i-1]);
-    return i-1;
-
+    Swap(v[pivot], v[i - 1]);
+    return i - 1;
 }
 
-void QuickSort(vector<int> &v, int pivot, int l, int r)
+void QuickSort(vector<int> &v, int pivot, int begin, int end)
 {
-    int m = Partition(v, pivot, l, r);
+    if (end <= begin)
+    {
+        return;
+    }
 
-    QuickSort(v, v[l], l, m-1) ;
-    QuickSort(v, v[m+1], m+1, r) ;
+    int m = Partition(v, pivot, begin, end); // m represents position of partition
+
+    QuickSort(v, begin, begin, m - 1);
+    QuickSort(v, m + 1, m + 1, end);
 }
 
-int main ()
+int main()
 {
-    string s; 
+    string s;
     getline(cin, s);
 
     int num = 0;
     vector<int> v;
 
     /*Taking input from the user and storing it in a vector of int*/
-    for(int i = 0; i < s.size(); ++i)
+    for (int i = 0; i < s.size(); ++i)
     {
-        if(s[i] == ' ')
+        if (s[i] == ' ')
             v.push_back(num), num = 0;
         else
-            num = num*10 + (s[i]-'0');
-    }   
+            num = num * 10 + (s[i] - '0');
+    }
     v.push_back(num);
 
-    int pivot = 0 ;     //Initially the pivot element is element at 0th index
+    int pivot = 0; // Initially the pivot element is element at 0th index
 
-    QuickSort(v, pivot, 0, v.size()-1) ;
+    QuickSort(v, pivot, 0, v.size() - 1);
 
-    for (int i = 0 ; i < v.size(); i++)
+    for (int i = 0; i < v.size(); i++)
     {
-        cout << v[i] << " " ;
+        cout << v[i] << " ";
     }
 
-    return 0 ;
+    return 0;
 }
