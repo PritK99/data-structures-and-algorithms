8b195a3af23bd3b98e1fc75b10357b54a2e5da3d
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2023-10-23 11:18:30 +0530

    Modifying structure of linked list

--- 04_Linked List/linked_list.cpp
+++ 04_Linked List/linked_list.cpp
@@ -1,154 +1,110 @@
-/*
-This program creates a linked list and preforms basic operations on it such as insertions of an element, deletion of an element etc.
-Note : Without using the tail pointer, time complexity to insert an element is o(n). However, we can do this in o(1) by using a tail pointer
-*/
 #include <iostream>
 
 using namespace std;
 
-class LinkedList
+class Node
 {
-    struct node
+    public:
+    int data;
+    Node* next;
+    Node(int data)
     {
-        int data;
-        struct node *next = NULL;
-    };
-    struct node *head = NULL;
-
-public:
-    void display();
-    void insert(int key);
-    void remove(int key);
+        this->data = data;
+        next = NULL;
+    }
 };
 
-void LinkedList ::display()
+class LinkedList
 {
+    public: 
+    Node* head = NULL;
+    void insert(int data);
+    void print();
+    void remove(int data);
+};
 
-    struct node *temp = head;
-
-    if (temp == NULL)
-    {
-        cout << "The list is empty\n";
-    }
-    else
-    {
-        cout << "Elements in linked list are : ";
-        while (temp != NULL)
-        {
-            cout << temp->data << " ";
-            temp = temp->next;
-        }
-
-        cout << endl;
-    }
-}
-
-void LinkedList ::insert(int key)
+void LinkedList::insert(int data)
 {
     if (head == NULL)
     {
-        struct node *temp = new node;
-        head = temp;
-        temp->data = key;
-        temp->next = NULL;
+        head = new Node(data);
     }
     else
     {
-        struct node *temp = head;
+        Node* temp = head;
         while (temp->next != NULL)
         {
             temp = temp->next;
         }
-        temp->next = new node;
-        temp = temp->next;
-        temp->data = key;
-        temp->next = NULL;
+        temp->next = new Node(data);
     }
 }
 
-void LinkedList ::remove(int key)
+void LinkedList::print()
 {
-    int found = 0;
-
-    if (head != NULL) // if LinkedList is not empty
+    if (head == NULL)
     {
-        struct node *temp = head;
-
-        if (temp->data == key) // if the first element of list is to be deleted
+        cout << "Linked List is Empty" << endl;
+    }
+    else
+    {
+        Node* temp = head;
+        while (temp != NULL)
         {
-            head = temp->next;
-            free(temp);
-            found = 1;
+            cout << temp->data << " -> ";
+            temp = temp->next;
         }
+        cout << "NULL" << endl;
+    }
+}
 
-        if (temp->next != NULL)
+void LinkedList::remove(int key)
+{
+    if (head == NULL)
+    {
+        cout << "Linked List is Empty" << endl;
+    }
+    else if(head->data == key)
+    {
+        Node* temp = head;
+        temp = head->next;
+        free(head);
+        head = temp;
+    }
+    else
+    {
+        bool flag = 0;
+        Node* temp = head;
+        while (temp->next != NULL)
         {
-            while (temp->next->next != NULL && found == 0) // if the element to be deleted is not the first or last element in linked list
+            if (temp->next->data == key)
             {
-                if (temp->next->data == key)
-                {
-                    struct node *p = temp->next;
-                    temp->next = temp->next->next;
-                    free(p);
-                    found = 1;
-                    break;
-                }
+                flag = 1;
+                Node* p = temp->next;
+                temp->next = temp->next->next;
+                free(p);
+                break;
+            }
+            else
+            {
                 temp = temp->next;
-            }
-
-            if (temp->next->data == key && found == 0)
-            {
-                free(temp->next);
-                found = 1;
-                temp->next = NULL;
-            }
+            }            
         }
-
-        if (found == 0)
+        if (flag == 0)
         {
-            cout << "Element not found\n";
+            cout << "Key Not Found" << endl;
         }
     }
 }
 
 int main()
 {
-    LinkedList l;
-
-    // menu driven program
-    int choice = 0, key;
-    while (1)
-    {
-        cout << "Choose 1 to insert an element to the linked list\n";
-        cout << "Choose 2 to delete an element from the linked list\n";
-        cout << "Choose 3 to display the linked list\n";
-        cout << "Choose 4 to exit\n";
-        cout << "Enter your choice : ";
-        cin >> choice;
-
-        if (choice == 4)
-        {
-            break;
-        }
-        else if (choice == 1)
-        {
-            cout << "Please enter the key element : ";
-            cin >> key;
-            l.insert(key);
-        }
-        else if (choice == 2)
-        {
-            cout << "Please enter the key element : ";
-            cin >> key;
-            l.remove(key);
-        }
-        else if (choice == 3)
-        {
-            l.display();
-        }
-
-        cout << endl;
-    }
-
+    LinkedList L;
+    L.insert(3);
+    L.insert(6);
+    L.insert(9);
+    L.print();
+    L.remove(9);
+    L.print();
     return 0;
 }
